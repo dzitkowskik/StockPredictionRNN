2c513129ce7d387b83a7afac8199cde180b62114
Karol Dzitkowski <k.dzitkowski@gmail.com>
2016-01-25 22:46:27 +0100

    little update

--- src/nyse-rnn/nyse.py
+++ src/nyse-rnn/nyse.py
@@ -358,7 +358,7 @@ def get_test_data(window_size):
 def main():
     book = NyseOpenBook("test")
 #     # filename = 'bigFile.binary'
-    filename = '/media/ghash/OTHER/Dane/EQY_US_NYSE_BOOK_20130403/openbookultraAA_N20130403_1_of_1'
+    filename = 'openbookultraAA_N20130403_1_of_1'
 #     # record_filter = (lambda x: ('NOM' in x.Symbol) & ((x.Side == 'B') | (x.Side == 'S')))
 #     # record_filter = (lambda x: 'AZN' in x.Symbol)
     record_filter = (lambda x: True)
@@ -367,7 +367,7 @@ def main():
 #     # book.print_records()
 # #     db_client = pymongo.MongoClient('localhost', 27017)
 # #     book.save_to_db(db_client['nyse'])
-#         
+#
     for list in book.symbols_dict.itervalues():
         book.pickle_to_file(list[0].Symbol)
     
